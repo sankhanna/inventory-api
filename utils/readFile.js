136a040edf22161e6b0b15342df947829f587987
sankhanna <sankhanna@gmail.com>
2025-10-16 07:13:19 +0530

    Fixes

--- utils/readFile.js
+++ utils/readFile.js
@@ -1,14 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-function get_file_content(file_name){
-    var file_content = "";
-    try {
-        var appDir = path.dirname(require.main.filename);
-        file_content = fs.readFileSync(appDir + "/presets/" + file_name , "utf8");
-    } 
-    catch (error) {}    
-    return file_content;
+function get_file_content(file_name) {
+  var file_content = "";
+  try {
+    var appDir = path.dirname(require.main.filename);
+    file_content = fs.readFileSync("../presets/" + file_name, "utf8");
+  } catch (error) {}
+  return file_content;
 }
 
 module.exports = get_file_content;
