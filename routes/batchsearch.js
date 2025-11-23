a62da675c36b5450a23506ba8789f06e9bcdc30f
sankhanna <sankhanna@gmail.com>
2025-11-23 10:08:49 +0530

    Fixed the batch search

--- routes/batchsearch.js
+++ routes/batchsearch.js
@@ -10,9 +10,29 @@ router.get("/", async (req, res) => {
   const pipeLine = [
     { $match: filters },
     { $addFields: { transactions: { $filter: { input: "$transactions", as: "transaction", cond: { $eq: ["$$transaction.batch_no", main_search] } } } } },
-    { $lookup: { from: "products", localField: "transactions.product_id", foreignField: "_id", as: "product_info" } },
+    { $unwind: { path: "$transactions", preserveNullAndEmptyArrays: true } },
+    {
+      $lookup: {
+        from: "products",
+        localField: "transactions.product_id",
+        foreignField: "_id",
+        as: "product_info",
+      },
+    },
     { $unwind: { path: "$product_info", preserveNullAndEmptyArrays: true } },
     { $addFields: { "transactions.product_name": "$product_info.product_name" } },
+    {
+      $group: {
+        _id: "$_id",
+        transactions: { $push: "$transactions" },
+        change_user_id: { $first: "$change_user_id" },
+        create_user_id: { $first: "$create_user_id" },
+        transaction_date: { $first: "$transaction_date" },
+        transaction_type: { $first: "$transaction_type" },
+        workshop_id: { $first: "$workshop_id" },
+        to_workshop_id: { $first: "$to_workshop_id" },
+      },
+    },
     { $lookup: { from: "accounts", localField: "account_id", foreignField: "_id", as: "account" } },
     { $lookup: { from: "workshops", localField: "workshop_id", foreignField: "_id", as: "workshop" } },
     { $lookup: { from: "workshops", localField: "to_workshop_id", foreignField: "_id", as: "toworkshop" } },
@@ -24,20 +44,19 @@ router.get("/", async (req, res) => {
     { $unwind: { path: "$changeUser", preserveNullAndEmptyArrays: true } },
     { $unwind: { path: "$createUser", preserveNullAndEmptyArrays: true } },
     { $project: { product_info: 0 } },
-    { $sort: { transaction_date: -1 } },
+    { $sort: { transaction_date: -1, _id: -1 } },
   ];
 
   const materialissue = await MaterialIssue.aggregate(pipeLine);
 
-  let records = [];
-  records = materialissue.map((itm) => {
+  const records = materialissue.map((itm) => {
     workshop_name = itm.workshop?.name || "";
     to_workshop_name = itm.toworkshop?.name || "";
     account_name = itm.account?.account_name || "";
     create_user_name = itm.createUser?.complete_name || "";
     change_user_name = itm.changeUser?.complete_name || "";
 
-    obj = {
+    return {
       _id: itm._id,
       workshop_id: itm.workshop_id,
       workshop_name,
@@ -54,8 +73,6 @@ router.get("/", async (req, res) => {
       create_user_name,
       transactions: itm.transactions,
     };
-
-    return obj;
   });
 
   if (records.length == 0) return res.status(SUCCESS).send(addMarkup(1, "Search result", { search_result: [] }));
